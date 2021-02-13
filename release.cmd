53aa5aae9cca7f13a4b28c0e59b28e8909ace5f0
JustOff <Off.Just.Off@gmail.com>
2021-02-13 22:18:31 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,12 @@
+@echo off
+set VER=1.0.9
+
+sed -i -E "s/\"version\": \".+?\"/\"version\": \"%VER%\"/; s/\"name\": \".+?\"/\"name\": \"advanced-night-mode-%VER%\"/" package.json
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/advanced-night-mode-.+?\.xpi/download\/%VER%\/advanced-night-mode-%VER%\.xpi/" update.xml
+
+set XPI=advanced-night-mode-%VER%.xpi
+if exist %XPI% del %XPI%
+if exist bootstrap.js del bootstrap.js
+if exist install.rdf del install.rdf
+call jpm xpi
+unzip %XPI% bootstrap.js install.rdf
